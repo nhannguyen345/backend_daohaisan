1fa2d638989a3e08a96bc224f5354fb3da6bb0c6
Tien Nhan <nhandzzcuteo@gmail.com>
2023-11-25 16:50:10 +0700

    Add module nodemailer

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 const orderController = require("../controllers/orderController");
-const { upload } = require("../config/setUpFirebase");
+const { upload } = require("../config/setupfirebase");
 const validateToken = require("../config/validateTokenHandler");
 
 //[GET] api/users
